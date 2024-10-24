f98adc376cf0294cf8d3c96642d31f46f75a4948
Jack <90474780+jto-d@users.noreply.github.com>
2024-10-24 01:45:10 -0400

    updated constants

--- frontend/src/constants.tsx
+++ frontend/src/constants.tsx
@@ -1,4 +1,5 @@
 /**
  * The base url of the target API
  */
-export const BASE_URL = "https://charter-assassin.onrender.com";
+export const BASE_URL = "https://api.charterassassin.com";
+// export const BASE_URL = "https://charter-assassin.onrender.com";
